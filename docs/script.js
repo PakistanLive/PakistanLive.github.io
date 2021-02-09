e7ff3bc0f0a2405434fe2cbdc0648f86e112dbf2
UsamaSarwar <UsamaSarwarOfficial@gmail.com>
2021-02-10 01:18:29 +0500

    updated

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,12 @@
-  var firebaseConfig = {
-    apiKey: "AIzaSyDD4nqrJMFXVsDIohpJ5Vf3oe8xgz8t78I",
-    authDomain: "pk360live.firebaseapp.com",
-    projectId: "pk360live",
-    storageBucket: "pk360live.appspot.com",
-    messagingSenderId: "436854299657",
-    appId: "1:436854299657:web:5756f55947adbb00e2acd8",
-    measurementId: "G-HS1ZCRV012"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+var firebaseConfig = {
+  apiKey: "AIzaSyBUT0DiByBXAUdNk-7qlG9qnc8PLshWm3A",
+  authDomain: "pakistanlive-tv.firebaseapp.com",
+  projectId: "pakistanlive-tv",
+  storageBucket: "pakistanlive-tv.appspot.com",
+  messagingSenderId: "530230976335",
+  appId: "1:530230976335:web:910badb3f9ead17722af3a",
+  measurementId: "G-3JCYP9X2KL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
